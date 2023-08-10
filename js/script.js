b5c2875dd344ec5802c3ad40d57f1d1474d8d11b
annaletyflor <annaletyflor@gmail.com>
2023-08-10 10:54:25 -0300

    Finalizado!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-var toggleMode = () =>{
-    const html = document.documentElement;
-    html.classList.toggle("light");
+var toggleMode = () => {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const isLight = html.classList.contains("light");
+  const srcValue = isLight ? "../img/SUS2.jpg" : "../img/SUS.jpg";
+  const altValue = isLight
+    ? "Avatar da Neeko, personagem de League of Legends com traços de camaleão na cor azul de inverno"
+    : "Avatar da Neeko, personagem de League of Legends com traços de camaleão na cor roxa e laranja, com roupa de bruxinha";
+
+  document.querySelector("#profile img").setAttribute("src", srcValue);
+  document.querySelector("#profile img").setAttribute("alt", altValue);
 };
